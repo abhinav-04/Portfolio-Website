bf1ddf7e667d83077ac1806563e8353323eeb32e
Abhinav Agarwal <abhinav@Abhinavs-MacBook-Air-3.local>
2022-11-26 19:06:57 +0530

    first commit

---
+++ assets/js/myQuery.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    $('#hh1').slideToggle().slideToggle(2000);
+
+    $('#img1').fadeToggle(10).fadeToggle(3000);
+    $('#desc').fadeToggle(10).fadeToggle(3000);
+    $('#down-butt').click(function(){
+        $('#down-butt').slideToggle().slideToggle();
+    });
+
+}
+);
